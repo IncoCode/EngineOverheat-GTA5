f2a2ebf8840ea49aa5c6d65a0180a0c192e35dfe
IncoCode <akulich.dimon@gmail.com>
2016-01-31 20:03:18 +0200

    Add ActionsEveryTick

--- Code/EngineOverheat/Model/TaskSequenceData.cs
+++ Code/EngineOverheat/Model/TaskSequenceData.cs
@@ -15,6 +15,7 @@ namespace EngineOverheat.Model
         public TaskSequence TaskSequence { get; private set; }
 
         private readonly Dictionary<int, List<Action>> _actions;
+        private readonly Dictionary<int, List<Action>> _actionsEveryTick;
         private int _lastFiredIndex = -1;
 
         public TaskSequenceData( Ped ped, TaskSequence taskSequence, bool isActive = true )
@@ -24,34 +25,55 @@ namespace EngineOverheat.Model
             this.IsActive = isActive;
 
             this._actions = new Dictionary<int, List<Action>>();
+            this._actionsEveryTick = new Dictionary<int, List<Action>>();
         }
 
-        public void FireWhenSequenceIndex( int sequenceIndex, Action action )
+        private void AddAction( Dictionary<int, List<Action>> actions, int sequenceIndex, Action action )
         {
             List<Action> actionsList;
-            if ( !this._actions.TryGetValue( sequenceIndex, out actionsList ) )
+            if ( !actions.TryGetValue( sequenceIndex, out actionsList ) )
             {
                 actionsList = new List<Action>();
-                this._actions.Add( sequenceIndex, actionsList );
+                actions.Add( sequenceIndex, actionsList );
             }
             actionsList.Add( action );
         }
 
+        private void FireActions( int sequenceIndex, Dictionary<int, List<Action>> actions )
+        {
+            List<Action> actionsList;
+            if ( !actions.TryGetValue( sequenceIndex, out actionsList ) )
+            {
+                return;
+            }
+            actionsList.ForEach( action => action.Invoke() );
+        }
+
+        public void FireWhenSequenceIndex( int sequenceIndex, Action action )
+        {
+            this.AddAction( this._actions, sequenceIndex, action );
+        }
+
+        public void FireWhenSequenceIndexEveryTick( int sequenceIndex, Action action )
+        {
+            this.AddAction( this._actions, sequenceIndex, action );
+        }
+
         public void Update()
         {
+            if ( !this.IsActive )
+            {
+                return;
+            }
             var sequenceIndex = this.Ped.TaskSequenceProgress;
-            if ( !this.IsActive || this._lastFiredIndex == sequenceIndex )
-            {
-                return;
-            }
+            this.FireActions( sequenceIndex, this._actionsEveryTick );
 
-            this._lastFiredIndex = sequenceIndex;
-            List<Action> actions;
-            if ( !this._actions.TryGetValue( sequenceIndex, out actions ) )
+            if ( this._lastFiredIndex == sequenceIndex )
             {
                 return;
             }
-            actions.ForEach( action => action.Invoke() );
+            this._lastFiredIndex = sequenceIndex;
+            this.FireActions( sequenceIndex, this._actions );
         }
     }
 }
