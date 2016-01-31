1be37f5a1fc77d1ed14f0221a69b77bd9a076a61
IncoCode <akulich.dimon@gmail.com>
2016-01-31 18:43:36 +0200

    Add TaskSequenceEvent

---
+++ Code/EngineOverheat/Model/TaskSequenceData.cs
@@ -0,0 +1,49 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using GTA;
+
+#endregion
+
+namespace EngineOverheat.Model
+{
+    public class TaskSequenceData
+    {
+        public bool IsActive;
+        public Ped Ped { get; private set; }
+        public TaskSequence TaskSequence { get; private set; }
+
+        private readonly Dictionary<int, List<Action>> _actions;
+
+        public TaskSequenceData( Ped ped, TaskSequence taskSequence, bool isActive = true )
+        {
+            this.TaskSequence = taskSequence;
+            this.Ped = ped;
+            this.IsActive = isActive;
+
+            this._actions = new Dictionary<int, List<Action>>();
+        }
+
+        public void FireWhenSequenceIndex( int sequenceIndex, Action action )
+        {
+            List<Action> actionsList;
+            if ( !this._actions.TryGetValue( sequenceIndex, out actionsList ) )
+            {
+                actionsList = new List<Action>();
+            }
+            actionsList.Add( action );
+        }
+
+        public void Update()
+        {
+            var sequenceIndex = this.Ped.TaskSequenceProgress;
+            List<Action> actions;
+            if ( !this._actions.TryGetValue( sequenceIndex, out actions ) )
+            {
+                return;
+            }
+            actions.ForEach( action => action.Invoke() );
+        }
+    }
+}
---
+++ Code/EngineOverheat/TaskSequenceEvent.cs
@@ -0,0 +1,47 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EngineOverheat.Model;
+using GTA;
+
+#endregion
+
+namespace EngineOverheat
+{
+
+    public class TaskSequenceEvent
+    {
+        #region Fields
+
+        public static TaskSequenceEvent Instance => _instance ?? ( _instance = new TaskSequenceEvent() );
+
+        #endregion
+
+        private readonly List<TaskSequenceData> _taskSequencesData;
+        private static TaskSequenceEvent _instance;
+
+        private TaskSequenceEvent()
+        {
+            this._taskSequencesData = new List<TaskSequenceData>();
+        }
+
+        public void Subscribe( int sequenceIndex, Ped ped, TaskSequence taskSequence, Action action )
+        {
+            TaskSequenceData taskSequenceData = this._taskSequencesData
+                .FirstOrDefault( tsd => tsd.Ped == ped && tsd.TaskSequence == taskSequence );
+            if ( taskSequenceData == null )
+            {
+                taskSequenceData = new TaskSequenceData( ped, taskSequence );
+                this._taskSequencesData.Add( taskSequenceData );
+            }
+            taskSequenceData.FireWhenSequenceIndex( sequenceIndex, action );
+        }
+
+        public void Update()
+        {
+            this._taskSequencesData.ForEach( taskSequence => taskSequence.Update() );
+        }
+    }
+}
